4c6586f130a3fdd53f02b21d256c7cb035d0289d
N-Heyer <156978640+N-Heyer@users.noreply.github.com>
2024-12-07 19:44:06 -0600

    revert changes

--- calc.cpp
+++ calc.cpp
@@ -6,13 +6,9 @@
 //no decimal support
 
 #include <iostream>
-#include <iostream>
-#include <cstring>
-#include <cctype>
-#include <cmath>
 using namespace std;
 
-const int bufferSize = 256;
+const int bufferSize = 30;
 bool divideByZero = false;
 
 //this function will get the input
@@ -41,16 +37,21 @@ void getInput(char input[]) {
 //remove all blank spaces from an array
 //input char[]
 //output char[]
-// Function to remove blanks from a string
-void removeBlank(char* str) {
-    int i = 0, j = 0;
-    while (str[i]) {
-        if (!isspace(str[i])) {
-            str[j++] = str[i];
+void removeBlank(char input[]) {
+    int blanksRemoved = 0;
+
+    for (int i = 0; i < bufferSize; ++i) {
+        input[i - blanksRemoved] = input[i];
+        if (input[i] == 0 || input[i] == '\0' || input[i] == ' ' || input[i] == 32) {
+            blanksRemoved++;
         }
-        i++;
     }
-    str[j] = '\0'; // Null-terminate
+    for (int i = bufferSize - blanksRemoved; i < bufferSize; ++i) {
+        input[i] = 0;
+
+    }
+    // Null-terminate the resulting string
+    //input[bufferSize- blanksRemoved-1] = '\0';
 }
 
 
